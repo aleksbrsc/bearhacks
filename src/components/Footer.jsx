8105fb4ec4a052d4f32f5e66d5e13c6ad26e562c
Sebastian Vega <sebasvega4604@gmail.com>
2025-03-19 13:46:14 -0400

    feat(footer): completed footer and added sponsor assets

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,24 @@
 import styles from '@css/components/footer.module.css';
-import logo from '@assets/images/logos/logo.svg';
+import logo from '@assets/images/logos/footer_logo.svg';
+import discord from "@assets/images/icons/discord.svg";
+import instagram from "@assets/images/icons/instagram.svg";
+import linkedin from "@assets/images/icons/linkedin.svg";
+import { Link } from 'react-router-dom';
 import { toast } from 'sonner'
 
 function Footer() {
     return (
         <footer id={styles.footer_section}>
             <div id={styles.footer_container}>
-
+                <img id={styles.footer_logo} src={logo} alt="BearHacks 2025 Logo" />
+                <div id={styles.footer_content}>
+                    <ul id={styles.socials_list}>
+                        <li className={styles.social_item}><Link to="" target='_blank'><img src={linkedin} alt="LinkedIn Page" /></Link></li>
+                        <li className={styles.social_item}><Link to="" target='_blank'><img src={instagram} alt="Instagram Page" /></Link></li>
+                        <li className={styles.social_item}><Link to="" target='_blank'><img src={discord} alt="Discord Server" /></Link></li>
+                    </ul>
+                    <p id={styles.code_of_conduct}><Link to="code-of-conduct">Code of Conduct</Link> &copy; BearHacks 2025</p>
+                </div>
             </div>
         </footer>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,14 +13,24 @@ const Home = () => {
     }, [buttonRef.current])
     return (
         <>
+                {/* {buttonLoaded && <HeroSketch button={buttonRef.current}/>} */}
             <section id={styles.hero_section}>
-                {buttonLoaded && <HeroSketch button={buttonRef.current}/>}
                 <div id={styles.hero_container}>
                     <div id={styles.hero_header}>
                         <h1>BearHacks 2025</h1>
                         <p>Sheridan’s first hybrid hackathon — <strong>March 28-30 @ HMC campus</strong></p>
                     </div>
                     <a href="https://bearhacks2025.devpost.com" target="_blank" ref={buttonRef}>Apply Now</a>
+                </div>
+            </section>
+            <section id={styles.countdown_section}>
+                <div id={styles.countdown_container}>
+                    {/* <img src="" alt="" /> */}
+                    <div>
+                        <div>
+
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
