cd58d7bb122ed91aab588e62c42d2af90eca32c2
Aleksandar Bursac <aleksbrsc@gmail.com>
2025-03-28 00:16:24 -0400

    fix: instagram link navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
                 <div id={styles.footer_content}>
                     <ul id={styles.socials_list}>
                         <li className={styles.social_item}><Link to="https://www.linkedin.com/company/bearhacks/" target='_blank'><img src={linkedin} alt="LinkedIn Page" /></Link></li>
-                        <li className={styles.social_item}><Link to="https://www.instagram.com/bearhacks/" target='_blank'><img src={instagram} alt="Instagram Page" /></Link></li>
+                        <li className={styles.social_item}><Link to="https://www.instagram.com/bear_hacks/" target='_blank'><img src={instagram} alt="Instagram Page" /></Link></li>
                         <li className={styles.social_item}><Link to="https://discord.gg/g4d9A5R9Jc" target='_blank'><img src={discord} alt="Discord Server" /></Link></li>
                     </ul>
                     <p id={styles.code_of_conduct}><Link to="https://drive.google.com/file/d/1hd4oMXKCUGher1Z24KiusJMOdPzLSyBM/view" target='_blank'>Code of Conduct</Link> &copy; BearHacks 2025</p>
